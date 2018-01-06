c205f1f89e4f23a444e2c686e8a8f453eab6a49d
Hwan Seob Lee <bestseob93@gmail.com>
2018-01-06 22:50:55 +0900

    Uint8Array error occured

--- src/components/Tiling/Tiling.js
+++ src/components/Tiling/Tiling.js
@@ -4,23 +4,49 @@ import React, { Component } from 'react';
 type Props = {}
 
 type State = {
-  isOpen: boolean
+  isOpen: boolean,
+  file: ?Object,
+  imageUrl: any,
 }
 
 class Tiling extends Component<Props, State> {
-  constructor() {
-    super();
+  state = {
+    isOpen: false,
+    file: null,
+    imageUrl: null
+  };
 
-    this.state = {
-      isOpen: false
+  handleFile = (ev: SyntheticInputEvent<HTMLInputElement>): void => {
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+
+    reader.onloadend = () => {
+      const result = reader.result || {};
+      console.log(typeof result);
+
+      const arrayBufferView = new Uint8Array(result);
+      console.log(arrayBufferView);
+      // const blob = new Blob([arrayBufferView], {type: realMimeType});
+
+      this.setState({
+        file,
+        imageUrl: reader.result
+      });
     };
+    console.log(this.state.file);
+
+    reader.readAsArrayBuffer(file);
   }
 
   render() {
     console.log(this.state.isOpen);
     return (
       <div>
-        hi
+        <input
+          type="file"
+          onChange={this.handleFile}
+        />
+        <img src={this.state.imageUrl} alt="ㅎ" />
       </div>
     );
   }
