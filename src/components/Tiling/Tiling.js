3e5b6769abaceb4c2c411940c3fd7bb4fcb40eef
Hwan Seob Lee <bestseob93@gmail.com>
2018-01-11 21:20:23 +0900

    getRealMimeType added

--- src/components/Tiling/Tiling.js
+++ src/components/Tiling/Tiling.js
@@ -16,24 +16,64 @@ class Tiling extends Component<Props, State> {
     imageUrl: null
   };
 
+  getRealMimeType = (reader: any): string => {
+    console.log(reader);
+    const arr: Array = (new Uint8Array(reader.result)).subarray(0, 4);
+    let header: string = '';
+    let realMimeType: string = '';
+    let i: number = 0;
+    const arrLength: number = arr.length;
+
+    for (i; i < arrLength; i += 1) {
+      header += arr[i].toString(16);
+    }
+    switch (header) {
+      case '89504e47':
+        realMimeType = 'image/png';
+        break;
+      case '47494638':
+        realMimeType = 'image/gif';
+        break;
+      case 'ffd8ffDB':
+      case 'ffd8ffe0':
+      case 'ffd8ffe1':
+      case 'ffd8ffe2':
+      case 'ffd8ffe3':
+      case 'ffd8ffe8':
+        realMimeType = 'image/jpeg';
+        break;
+      default:
+        realMimeType = 'unknown'; // Or you can use the blob.type as fallback
+        break;
+    }
+
+    return realMimeType;
+  }
+
   handleFile = (ev: SyntheticInputEvent<HTMLInputElement>): void => {
     const reader = new FileReader();
     const file = ev.target.files[0];
 
     reader.onloadend = () => {
-      const result: object = reader.result || {};
-      console.log(typeof result);
+      console.log('1');
+      const result: any = reader.result || {};
+      const realMimeType = this.getRealMimeType(reader);
 
-      const arrayBufferView = new Uint8Array(result);
-      console.log(arrayBufferView);
-      // const blob = new Blob([arrayBufferView], {type: realMimeType});
-
-      this.setState({
-        file,
-        imageUrl: reader.result
-      });
+      if (realMimeType !== 'unknown') {
+        const arrayBufferView = new Uint8Array(result);
+        const blob = new Blob([arrayBufferView], { type: realMimeType });
+        const urlCreator = window.URL || window.webkitURL || {}.createObjectURL;
+        const imageUrl = urlCreator.createObjectURL(blob);
+        console.log(arrayBufferView);
+        this.setState({
+          ...this.state,
+          file,
+          imageUrl
+        });
+      } else {
+        alert('Please upload a valid image file');
+      }
     };
-    console.log(this.state.file);
 
     reader.readAsArrayBuffer(file);
   }
